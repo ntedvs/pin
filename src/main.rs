4e502d2cbce69d58264c88a66e40046a67cefa75
Nate <nate@qstreet.org>
2025-11-28 22:54:30 -0500

    add winner and place functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Copy)]
 struct Game {
     board: [u8; 9],
 }
@@ -30,6 +31,37 @@ impl Game {
             .filter(|(_, v)| **v == 0)
             .map(|(i, _)| i)
             .collect()
+    }
+
+    fn winner(&self) -> Option<u8> {
+        const LINES: [[usize; 3]; 8] = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for line in LINES {
+            let [a, b, c] = line;
+            let spot = self.board[a];
+
+            if spot != 0 && spot == self.board[b] && spot == self.board[c] {
+                return Some(spot);
+            }
+        }
+
+        None
+    }
+
+    fn place(&self, index: usize, player: u8) -> Game {
+        let mut new = *self;
+
+        new.board[index] = player;
+        new
     }
 }
 
